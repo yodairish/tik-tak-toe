3cb958784a96be760505e3fd2e2b097cf254d67c
yodairish <yodairish@gmail.com>
2015-01-25 23:09:00 +0000

    base of the project

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var gulp = require('gulp'),
+    pf = require('./js/paramFiles'),
+    // js
+    jshint = require('gulp-jshint'),
+    jscs = require('gulp-jscs'),
+    webpack = require('gulp-webpack'),
+    // CONSTANTS
+    JS_PATH = 'js',
+    PUBLIC_JS = 'public/js',
+    JS_ENTRY_POINTS = [
+      JS_PATH + '/main.js'
+    ];
+
+/**
+ * Linting JS files
+ * Possible send files to param -files=...
+ */
+gulp.task('jslint', function() {
+  var files = pf.paramFiles();
+
+  if (!files) {
+    files = [pf.js(JS_PATH)];
+  }
+
+  return gulp.src(files)
+    .pipe(jshint.extract())
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(jscs())
+    .pipe(jshint.reporter('fail'));
+});
+
+/**
+ * Processing JS files
+ */
+gulp.task('js', ['jslint'], function() {
+  return gulp.src(JS_ENTRY_POINTS)
+    .pipe(webpack())
+    .pipe(gulp.dest(PUBLIC_JS));
+});
+
+gulp.task('default', ['js']);
